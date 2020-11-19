cf07a1bad2ce5baa4db3b1ccc3ac0a626297ac01
ziv040299 <ziv04021999@gmail.com>
2020-11-19 13:47:37 +0200

    added if with nothing

---
+++ main_person.py
@@ -0,0 +1,9 @@
+from Person import Person
+person1 = Person()
+person1.name = input("Enter name: ")
+person1.age = int(input("Enter age: "))
+person1.childrens = int(input("Enter number of childrens: "))
+person1.show_details()
+print(person1.age_group())
+if 1==1:
+    pass
